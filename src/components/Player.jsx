f5ac82475603877ab50b5182d82930a5ffc3271f
SpandanPriyam22 <spandanpriyam1976@gmail.com>
2024-06-15 23:36:15 +0530

    New features

---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+
+export default function Player({name, symbol}){
+    const [currname, handleNamechange] = useState(name);
+    const [flag, setStatus] = useState(true);
+
+    const playerEntry = ()=>{
+        setStatus(!flag);
+    }
+
+    const nameChange = (event)=>{
+        handleNamechange(event.target.value);
+    }
+    
+    let status;
+    let tabContent;
+    if(flag){
+        status = "Edit";
+        tabContent = <span className="player-name">{currname}</span>;
+    }else{
+        status = "Save";
+        tabContent = <input type="text" required value={currname} onChange ={nameChange}></input>;
+    }
+
+    return(
+        <>
+        <li>
+            <span className = "player">
+                {tabContent}
+                <span className = "player-symbol">{symbol}</span>
+            </span>
+            <button onClick = {playerEntry}>{status}</button>
+        </li>
+        </>
+    );
+}
